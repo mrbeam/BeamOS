207cd4a26101d43c31fee7b45b16568fc74431f8
make-ing <bckr.florian@gmail.com>
2017-07-17 17:32:44 +0200

    added custom led blink stuff for my local build :)

--- src/vagrant/run_vagrant_mrbeam_build.sh
+++ src/vagrant/run_vagrant_mrbeam_build.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
+if [ $(hostname) = "denkbrett" ]; then
+    /opt/blink1/commandline/blink1-tool -m 100 --yellow
+fi;
+
 sudo vagrant ssh -- -t "sudo /OctoPi/src/variants/mrbeam/build_script.sh mrbeam $1"
+
+if [ $(hostname) = "denkbrett" ]; then
+    if [ $? -eq 0 ]; then
+        /opt/blink1/commandline/blink1-tool -t 1000 --green --blink 5
+        /opt/blink1/commandline/blink1-tool -m 500 --green
+    else
+        /opt/blink1/commandline/blink1-tool -t 1000 --red --blink 5
+        /opt/blink1/commandline/blink1-tool -m 500 --red
+    fi;
+fi;
