492e79133ca31ac6e28a42ddec869726149b4afc
Amrith Venkat Kesavamoorthi <amrith@mr-beam.org>
2024-01-26 09:42:08 +0100

    SW-4269 pylint * run pylint locally. * resolve formatting warnings

--- src/modules/beamos/filesystem/migration/usr/bin/sanitize_npz.py
+++ src/modules/beamos/filesystem/migration/usr/bin/sanitize_npz.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+""" Module to santitize npz files to be compatible with python 3.x """
 import os
 import numpy as np
 
@@ -10,25 +10,24 @@ BUSTER_FACTORY_LENS_CALIBRATION="factory_lens_correction.npz"
 LEGACY_FACTORY_LENS_CALIBRATION="lens_correction_2048x1536.npz"
 USER_LENS_CALIBRATION="lens_correction.npz"
 
-LENS_CALIBRATTION_FILES=[BUSTER_FACTORY_LENS_CALIBRATION, LEGACY_FACTORY_LENS_CALIBRATION, USER_LENS_CALIBRATION]
+LENS_CALIBRATTION_FILES=[
+    BUSTER_FACTORY_LENS_CALIBRATION, LEGACY_FACTORY_LENS_CALIBRATION, USER_LENS_CALIBRATION]
 
 
-def sanitize_npz(npz_file) -> int:
+def sanitize_npz(npz_file) -> None:
     """Sanitize npz files to be compatible with python 3.x"""
     print("Sanitizing: " + npz_file)
     try:
         data = np.load(npz_file, encoding='latin1', allow_pickle=True)
-    except Exception as e:
-        print("Error loading file: " + str(e))
-        return 1
+    except np.lib.format.FormatError as _e:
+        print("Error loading file: " + str(_e))
     np.savez_compressed(npz_file, **data)
-    return 0
 
 
 def sanitize_npz_files() -> None:
     """Sanitize all npz files in the cam directory"""
-    for f in LENS_CALIBRATTION_FILES:
-        npz_file = os.path.join(PRESERVE_DATA_CAM_DIRECTORY, f)
+    for _f in LENS_CALIBRATTION_FILES:
+        npz_file = os.path.join(PRESERVE_DATA_CAM_DIRECTORY, _f)
         if os.path.isfile(npz_file):
             # create backup retain the original file
             os.rename(npz_file, npz_file+".original")
