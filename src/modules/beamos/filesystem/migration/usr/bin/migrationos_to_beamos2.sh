5b4780a0439c74b631e513717c963a4dceeeddf4
amrithvenkat <110397957+amrithvenkat@users.noreply.github.com>
2024-02-19 12:47:49 +0100

    SW-4389 Add precondition check for sd card size (#123)
    * Check for sd-card size greater than 12GB to migrate.
    * Introduce exit codes for each stage associated with colors on do_exit().
    * Remove stray exit 0.
    * Catch exit codes on error and send exit 100 to to flash orange on phase 2.
    * Amend wrong parameter to mount function call

--- src/modules/beamos/filesystem/migration/usr/bin/migrationos_to_beamos2.sh
+++ src/modules/beamos/filesystem/migration/usr/bin/migrationos_to_beamos2.sh
@@ -14,9 +14,6 @@ BASEDIR="/usr/bin"
 #   Set LED Status Success/Fail
 #   Shutdown the device
 
-# Exit immediately if a command exits with a non-zero status.
-set -e
-
 timestamp()
 {
  date +"%Y-%m-%d %T"
@@ -28,6 +25,7 @@ do_exit()
   echo "$(timestamp) $0: Exitting. ${RET_CODE}"
   if [ "$RET_CODE" -eq 0 ]; then
     echo "$0: Normal exiting."
+    sudo bash ${BASEDIR}/migration.sh set-status success
   else
     echo "$0: Exiting with error code [${RET_CODE}]"
     sudo bash ${BASEDIR}/migration.sh set-status fail orange
@@ -53,20 +51,40 @@ trap do_exit EXIT
 echo "$(timestamp) $0: Flashing the SD-Card"
 sudo bash ${BASEDIR}/migration.sh set-status in-progress orange
 sudo bash ${BASEDIR}/migration.sh flash beamos2 sd-card
+exit_code=$?
+if [ "$exit_code" -ne 0 ]; then
+  echo "$(timestamp) $0: Flashing the SD-Card failed"
+  exit 100
+fi
 
 #   Mount the SD-Card
 echo "$(timestamp) $0: Mounting the SD-Card"
 sudo bash ${BASEDIR}/migration.sh set-status in-progress orange
-sudo bash ${BASEDIR}/migration.sh mount sd-cards
+sudo bash ${BASEDIR}/migration.sh mount sd-card
+exit_code=$?
+if [ "$exit_code" -ne 0 ]; then
+  echo "$(timestamp) $0: Mounting the SD-Card failed"
+  exit 100
+fi
 
 #   Restore Sensitive Data
 echo "$(timestamp) $0: Restoring Sensitive Data"
 sudo bash ${BASEDIR}/migration.sh set-status in-progress orange
 sudo bash ${BASEDIR}/migration.sh restore-data
+exit_code=$?
+if [ "$exit_code" -ne 0 ]; then
+  echo "$(timestamp) $0: Restoring Sensitive Data failed"
+  exit 100
+fi
 
 # Shutdown the device
 echo "$(timestamp) $0: Shutdown the device"
 sudo bash ${BASEDIR}/migration.sh set-status success
 sudo bash ${BASEDIR}/migration.sh shutdown
+exit_code=$?
+if [ "$exit_code" -ne 0 ]; then
+  echo "$(timestamp) $0: Shutdown the device failed"
+  exit 100
+fi
 
 exit 0
